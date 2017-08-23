3020340141cd201f9b00b3691c0b776868af8f26
sanzstv <steviesanz@comcast.net>
2017-08-23 15:49:24 -0700

    cross origin bs

--- src/app.js
+++ src/app.js
@@ -1,39 +1,88 @@
 (function(){
 	'use strict';
-	var Spotify = require('spotify-web-api-js');
-	var s = new Spotify();
-	var spotifyApi = new SpotifyWebApi();
-	/*spotifyApi.setAccessToken('');*/
-
-	angular.module('MusicSearchApp', [])
+ 
+	angular.module('MusicSearchApp',[])
 	.controller('MusicSearchController', MusicSearchController)
-	.factory('MusicService', MusicService);
+	.service('MusicSearchService', MusicSearchService)
+	.constant('API_BASE', "https://api.spotify.com");
 
-	MusicSearchController.$inject = ['$scope', '$location'];
-	function MusicSearchController($scope, $location){
-		/*initially for testing*/
-		var options = [
-			"Sonic Youth",
-			"Joy Division",
-			"Nine Inch Nails",
-			"The Velvet Underground",
-			"Siouxsie and the Banshees",
-			"Nick Cave and the Bad Seeds",
-			"Cocteau Twins",
-			"Radiohead",
-			"Talking Heads",
-			"Aphex Twin",
-			"My Bloody Valentine"
-		];
+	var api_id;
+    var api_secret;
+    var access_token;
+    var auth_code;
+    $.getJSON("config.json", function (data) {
+    	api_id=data.SPOTIFY_CLIENT_ID;
+    	api_secret=data.SPOTIFY_CLIENT_SECRET;
+    });
 
-		var start = Math.floor(Math.random() * options.length);
+	MusicSearchController.$inject = ["$http", 'MusicSearchService'];
+	function MusicSearchController($http, MusicSearchService){
+	    /*Authenticate user*/
+	    $http({
+	    	url: ("https://accounts.spotify.com/authorize"),
+	    	params: { 
+	    		client_id: api_id,
+	    		redirect_uri: "http://localhost/",
+	    		response_type: "code"
+	    	}
+	    }).then(function(response){
+	    	auth_code = response.code;
+	    });
 
-			spotifyApi.searchArtists('Hole')
-			  .then(function(data) {
-			    console.log('Search artists by "Love"', data);
-			  }, function(err) {
-			    console.error(err);
-			  });
+		/*Now receive access token from Spotify API*/
+		console.log(auth_code);
+		var artists = this;
+		artists.query = "";
+		artists.matches = [];
+		artists.message = "";
+		artists.search = function(){
+			if(artists.query){
+				var promise = MusicSearchService.getArtist(artists.query);
+				promise.then(function(response){
+					artists.matches = response;
+					if (artists.matches.length == 0){
+						artists.message = "No matches for \""+ artists.query +"\" ";
+					}
+					else{
+						artists.message = "All results for \""+ artists.query +"\" (" +artists.matches.length +" results)";
+					}
+				});
+			}
+			else{
+				artists.message = "Please enter a search string.";
+			}
+		};
+
+
 	}
+	MusicSearchService.$inject =["$http", "API_BASE"];
+	function MusicSearchService($http, API_BASE){
+		var service = this;
+		//find items from API and return matches
+		service.getArtist = function(query){
+			console.log(access_token);
+			/*return $http({
+				method: "GET",
+				url: (API_BASE + "/search"),
+				headers: {
+				    'Authorization': 'Bearer ' + access_token
+				},
+				params: {
+					q: query, 
+					type: "artist"
+				}
+			}).then(function (result) {
+			    var response = result.data;
+			    console.log(response);
+			    return response.filter(function(item){
+				    return item !== -1;
+				});
+			
+			});*/
+		};
+	}
+
+
+
 
 })();
